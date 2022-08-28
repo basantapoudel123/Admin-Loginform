1910ad907827f6466f067ea4cebe2f775187851c
basantapoudel123 <basanta.poudel1221@gmail.com>
2022-08-28 22:20:00 +0545

    complete login form for admin

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import LoginForm from "./components/LoginForm";
+import "./index.css"
+
+function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin@123"
+  }
+
+  const [user, setUser] = useState({name:"", email:""});
+  const[error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+  
+    if(details.email === adminUser.email && details.password === adminUser.password){
+      console.log("Logged In !!")
+      setUser ({
+        name: details.name,
+        email: details.email
+      })
+    }else{
+      console.log("Details do not Match !!!");
+      setError("Details donot Matched !!");
+    }
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ""})
+  }
+
+  return (
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  )
+}
+
+export default App
